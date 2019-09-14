d6789fe888c09e2376f0c4af4b8b3bc0e888ba0e
Oliver Wegner <void1976@gmail.com>
2019-09-14 22:18:25 +0200

    working on code

--- src/clj_brokee/core.clj
+++ src/clj_brokee/core.clj
@@ -1,6 +1,25 @@
-(ns clj-brokee.core)
+(ns clj-brokee.core
+  (:require [clojure.core.async :as a]))
+
+(defprotocol MessageBroker
+  (get-producer [this topic])
+  (subscribe [this topic group-id]))
+
+(defrecord CoreAsyncMessageBroker []
+
+  MessageBroker
+
+  (get-producer [this topic]
+    (a/chan))
+
+  (subscribe [this topic group-id]
+    (a/chan)))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn -main [& args]
+  (println "aaaaaaaaaaa")
+  (foo 123))
