6638b041aa01e3b9e91cbb20490f5f43d9373c10
Oliver Wegner <void1976@gmail.com>
2019-09-18 18:21:07 +0200

    playing around with abstraction ideas

--- src/clj_brokee/core.clj
+++ src/clj_brokee/core.clj
@@ -1,6 +1,8 @@
 (ns clj-brokee.core
   (:require [clojure.core.async :as a]))
 
+;;; NOTE: this is just a playground for now
+
 (defprotocol Broker
   (producer-channel [this])
   (consumer-channel [this topic group-id])
@@ -26,3 +28,85 @@
 (defprotocol Adapter
   (recv-ch [this ch])
   (send-ch [this ch]))
+
+
+
+
+(defn user-xf [determine-user-fn]
+  )
+
+(defn permission-validating-xf [check-permissions-fn]
+  (filter (fn [msg]
+            (check-permissions-fn msg))))
+
+(defn dispatching-xf []
+  )
+
+
+
+(defprotocol EventBroker
+  (reg-event-db [this id handler])
+  (reg-sub [this id handler])
+  (dispatch [this event]))
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;
+
+(defprotocol Receiver
+  (tx-ch [this]))
+
+(defprotocol Sender
+  (rx-ch [this]))
+
+
+(defrecord Backbone [mix mult]
+  Receiver
+  (tx-ch [this]
+    (let [ch (a/chan)]
+      (a/admix mix ch)
+      ch))
+  Sender
+  (rx-ch [this]
+    (let [ch (a/chan)]
+      (a/tap mult ch)
+      ch)))
+
+(defn connect [{:keys [mix mult] :as this} other]
+  (when (satisfies? Sender other)
+    (a/admix mix (rx-ch other)))
+  (when (satisfies? Receiver other)
+    (a/tap mult (tx-ch other)))
+  this)
+
+(defn construct [this]
+  (map->Backbone {}))
+
+(defn start [this]
+  (let [ch   (a/chan)
+        mix  (a/mix ch)
+        mult (a/mult ch)]
+    (assoc this
+           :mix  mix
+           :mult mult)))
+
+
+(defrecord WebsocketAdapter []
+  Sender
+  (tx-ch [this])
+  Receiver
+  (rx-ch [this]))
+
+(defrecord ReFrameAdapter []
+  Sender
+  (tx-ch [this])
+  Receiver
+  (rx-ch [this]))
+
+
+(defrecord Client [backbone
+                   backbone-tx-ch backbone-rx-ch])
+(defn client-foo [{:keys [backbone-tx-ch backbone-rx-ch] :as this} x]
+  (a/put! backbone-tx-ch {:x x})
+  (a/<!! backbone-rx-ch))
