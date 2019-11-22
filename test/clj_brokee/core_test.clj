0362a4c2bb3d84679ca8cae7d8d727348e2a9097
Oliver Wegner <void1976@gmail.com>
2019-11-22 15:49:07 +0100

    working on tests

--- test/clj_brokee/core_test.clj
+++ test/clj_brokee/core_test.clj
@@ -5,7 +5,32 @@
             [clj-brokee.producer :as p]
             [clj-brokee.consumer :as c]))
 
-(deftest hub
+(def message1 {:foo "foo"
+               :bar 123
+               :baz [{:a 1.23}
+                     {:b #{:x :y :z}}]})
+(def message2 {:foo "foo2"
+               :bar 1234
+               :baz [{:a 1.234}
+                     {:b #{:x :y :z}}]})
+
+(deftest via-broker
+  (let [topic    :topic1
+        broker   (-> (b/construct)
+                     (b/start))
+        producer (b/producer broker)
+        consumer (b/consumer broker topic)]
+    (testing "send & receive message via broker"
+      (future
+        (p/produce producer topic message1)
+        (p/produce producer topic message2))
+      (is (= message1
+             (c/consume consumer)))
+      (is (= message2
+             (c/consume consumer))))
+    (b/stop broker)))
+
+(deftest via-hub
   (let [topic    :topic1
         broker1  (-> (b/construct)
                      (b/start))
@@ -17,13 +42,14 @@
                      (h/connect broker2))
         producer (b/producer broker1)
         consumer (b/consumer broker2 topic)]
-    (testing "via hub"
-      (let [msg {:foo "foo"
-                 :bar 123
-                 :baz {:x 1.23}}]
-        (p/produce producer topic msg)
-        (is (= msg
-               (c/consume consumer)))))
+    (testing "send & receive message brokers & hub"
+      (future
+        (p/produce producer topic message1)
+        (p/produce producer topic message2))
+      (is (= message1
+             (c/consume consumer)))
+      (is (= message2
+             (c/consume consumer))))
     (h/stop hub)
     (b/stop broker2)
     (b/stop broker1)))
