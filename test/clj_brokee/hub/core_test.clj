88f1a231a6bb9b89e82384a113a18a340cbb6a3d
Oliver Wegner <void1976@gmail.com>
2019-12-10 16:29:53 +0100

    adjust tests for hub & transport

--- test/clj_brokee/hub/core_test.clj
+++ test/clj_brokee/hub/core_test.clj
@@ -6,15 +6,20 @@
 (deftest hub
   (let [h  (h/construct)
         a1 (atom [])
-        a2 (atom [])]
-    (h/listen h (fn [msg]
-                  (swap! a1 conj msg)))
-    (h/listen h (fn [msg]
-                  (swap! a2 conj msg)))
-    (h/emit h :x)
-    (h/emit h :y)
-    (is (= [:x :y] @a1))
-    (is (= [:x :y] @a2))))
+        a2 (atom [])
+        a3 (atom [])
+        c1 (h/plug-in h (fn [msg]
+                          (swap! a1 conj msg)))
+        c2 (h/plug-in h (fn [msg]
+                          (swap! a2 conj msg)))
+        c3 (h/plug-in h (fn [msg]
+                          (swap! a3 conj msg)))]
+    (h/emit c1 :x)
+    (h/emit c2 :y)
+    (h/emit c3 :z)
+    (is (= [:y :z] @a1))
+    (is (= [:x :z] @a2))
+    (is (= [:x :y] @a3))))
 
 (deftest direct-transport
   (let [h1  (h/construct)
@@ -23,22 +28,32 @@
                 (td/start))
         a11 (atom [])
         a12 (atom [])
+        a13 (atom [])
         a21 (atom [])
-        a22 (atom [])]
-    (h/listen h1 (fn [msg]
-                   (swap! a11 conj msg)))
-    (h/listen h1 (fn [msg]
-                   (swap! a12 conj msg)))
-    (h/listen h2 (fn [msg]
-                   (swap! a21 conj msg)))
-    (h/listen h2 (fn [msg]
-                   (swap! a22 conj msg)))
-    (h/emit h1 :x1)
-    (h/emit h1 :y1)
-    (h/emit h2 :x2)
-    (h/emit h2 :y2)
-    (is (= [:x1 :y1 :x2 :y2] @a11))
-    (is (= [:x1 :y1 :x2 :y2] @a12))
-    (is (= [:x1 :y1 :x2 :y2] @a21))
-    (is (= [:x1 :y1 :x2 :y2] @a22))
+        a22 (atom [])
+        a23 (atom [])
+        c11 (h/plug-in h1 (fn [msg]
+                            (swap! a11 conj msg)))
+        c12 (h/plug-in h1 (fn [msg]
+                            (swap! a12 conj msg)))
+        c13 (h/plug-in h1 (fn [msg]
+                            (swap! a13 conj msg)))
+        c21 (h/plug-in h2 (fn [msg]
+                            (swap! a21 conj msg)))
+        c22 (h/plug-in h2 (fn [msg]
+                            (swap! a22 conj msg)))
+        c23 (h/plug-in h2 (fn [msg]
+                            (swap! a23 conj msg)))]
+    (h/emit c11 :c11)
+    (h/emit c12 :c12)
+    (h/emit c13 :c13)
+    (h/emit c21 :c21)
+    (h/emit c22 :c22)
+    (h/emit c23 :c23)
+    (is (= [:c12 :c13 :c21 :c22 :c23] @a11))
+    (is (= [:c11 :c13 :c21 :c22 :c23] @a12))
+    (is (= [:c11 :c12 :c21 :c22 :c23] @a13))
+    (is (= [:c11 :c12 :c13 :c22 :c23] @a21))
+    (is (= [:c11 :c12 :c13 :c21 :c23] @a22))
+    (is (= [:c11 :c12 :c13 :c21 :c22] @a23))
     (td/stop t)))
